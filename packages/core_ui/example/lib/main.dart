1c8e0191b7f875fc48ee53e7f04b7328d039e99a
Nosa Daniel <33612084+nosadaniel@users.noreply.github.com>
2024-10-30 12:53:30 +0100

    fix(core_ui): remove unnecessary comment

--- packages/core_ui/example/lib/main.dart
+++ packages/core_ui/example/lib/main.dart
@@ -47,35 +47,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: SingleChildScrollView(
@@ -86,15 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
               style: Theme.of(context).appTextStyles.appTextStyle.displaySmall,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context)
-                  .appTextStyles
-                  .appTextStyle
-                  .displayLarge
-                  .copyWith(
-                      color: Theme.of(context).appColors.appColor.primary),
-            ),
+
             Text(
               Theme.of(context).appIcons.appIcon.character.search,
               style: TextStyle(
@@ -124,11 +92,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
