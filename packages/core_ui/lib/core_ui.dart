8ee6cbac4555b6b3e0c1be65b5136aebc005f196
Nosa Daniel <33612084+nosadaniel@users.noreply.github.com>
2024-11-11 16:36:43 +0100

    fix(core_ui): export reusable widget

--- packages/core_ui/lib/core_ui.dart
+++ packages/core_ui/lib/core_ui.dart
@@ -7,16 +7,22 @@ export './foundation/themes/app_icons_theme.dart';
 export './foundation/themes/app_images_theme.dart';
 //icons
 
-//
+///export molecules
+export './molecules/custom_app_bar.dart';
+export './molecules/category_card.dart';
+export './molecules/avatar.dart';
+export './molecules/app_text.dart';
 
 //buttons
 export './molecules/app_text_button.dart';
 export './molecules/centered_text_button.dart';
 
-//custom app bar
-export 'molecules/custom_app_bar.dart';
+///export organisms
+export "./organisms/empty_placeholder.dart";
+export './organisms/empty_content.dart';
+export './organisms/error_message.dart';
+export './organisms/responsive_center.dart';
+export './organisms/responsive_scrollable_card.dart';
 
-//category card
-export './molecules/category_card.dart';
 //extensions
 export './foundation/themes/extension.dart';
